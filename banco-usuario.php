664aef337c994d91ea39230110a57a4a26da9c4e
ronaldozacarias <ronaldo.rzs@gmail.com>
2020-05-26 14:35:00 -0300

    Login com sha256

--- banco-usuario.php
+++ banco-usuario.php
@@ -2,10 +2,11 @@
 require_once 'conecta.php';
 function buscaUsuario($conexao, $login, $senha)
 {
-	$senhaMd5 = md5($senha);
+	//$senhaMd5 = md5($senha);
+	$senhaSha256 = hash('sha256',$senha);
 	$login = mysqli_real_escape_string($conexao, $login);
 	$query = "select * from usuarios where login='{$login}'
-	and senha='{$senhaMd5}'";
+	and senha='{$senhaSha256}'";
 	$resultado = mysqli_query($conexao, $query);
 	$usuario = mysqli_fetch_assoc($resultado);
 	return $usuario;
